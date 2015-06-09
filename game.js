ac57da488129560f7c92b949d58b657897fb1873
carolw21 <carolw2000@gmail.com>
2015-06-09 15:10:52 -0700

    Yay.

---
+++ game.js
@@ -0,0 +1,15 @@
+function qScreen(){
+    setTimeout(function() {
+        window.location = 'gamescreen.html';
+    }, 0);
+}
+function rScreen(){
+    setTimeout(function() {
+        window.location = 'resultscreen.html';
+    }, 0);
+}
+function mainScreen(){
+	setTimeout(function() {
+        window.location = 'index.html';
+    }, 0);
+}
